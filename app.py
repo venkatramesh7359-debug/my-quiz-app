023d055b7ac65711e7127823391d38afe5851f9b
venkatramesh7359-debug <venkat.ramesh7359@gmail.com>
2026-02-18 21:03:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-st.set_page_config(page_title="https://docs.google.com/spreadsheets/d/17ErdXLapXbTPCFpitqZErZIV32nE0vcYTqcFO7Ip-Lg/edit?pli=1&gid=0#gid=0", page_icon="📚")
+st.set_page_config(page_title="Venkat Quiz App", page_icon="📚")
 st.title("📚 Venkat's Learning App")
-st.write("హలో! ఇది మీ మొదటి వెబ్ యాప్.")
 
-name = st.text_input("మీ పేరు:")
-if st.button("Start"):
-    st.balloons() # ఆకాశంలో బెలూన్లు వస్తాయి!
-    st.success(f"వెల్కమ్ {name}!")
+SHEET_URL = ""
+
+try:
+conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(spreadsheet=SHEET_URL, worksheet="Sheet1")
+
+except Exception as e:
+st.error("షీట్ కనెక్ట్ కాలేదు. Secrets మరియు Requirements ఫైల్స్ చెక్ చేయండి.")
